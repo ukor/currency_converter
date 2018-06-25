5aa3f69452488c1caaa7bace05213f792f5d8239
Ukor Jidechi E <ukorjidechi@gmail.com>
2018-06-25 05:24:48 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "../service_worker";
+import "../w3.css";
+
+class App extends Component{
+	render()
+	{
+		return (
+			<div>
+				<h1 className="w3-blue" >Currency Converter</h1>
+				<p>React wthout create react app.</p>
+			</div>
+		);
+	}
+}
+
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+
+module.exports = {
+	entry: "./src/index.js",
+	output: {
+		path: path.join(__dirname, "/build"),
+		filename: 'bundle.js'
+	},
+	module:
+	{
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{ loader: "style-loader" },
+					{ loader: "css-loader" }
+				  ]
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./public/index.html"
+		})
+	]
+}
